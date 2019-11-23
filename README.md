9198f23a596892214f0b5db4d50c47f8fa0dd259
ch264 <christinavhastenrath@gmail.com>
2019-11-23 11:53:28 -0800

    rewrite create element and render from react to vanilla JS

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BYO_React
-# BYO_React
+
+Following a tutorial on how to build your own react basic features
+
+https://pomb.us/build-your-own-react/
---
+++ react.js
@@ -0,0 +1,25 @@
+// Definition React element: object with 2 properties: types and props
+
+// define react element and rewrite in vanilla JS. Babel transpiles jsx to js 
+// const element = <h1 title="my react app">Hi there</h1>
+//  creates object from its arguments
+const element = React.createElement({
+	type: "h1", // type of DOM node we want to create (tagname) can also be a function
+	props: { // object from jsx attribute and has special array prop: children
+		title: "my react app",
+		childred: "Hi there",
+	},
+)
+
+// get node from DOM
+const container = document.getElementById("root");
+
+// render element in container
+// ReactDom.render(element, container);
+
+const node = document.createElement(element.type); // create node
+node["title"] = element.props.title; // assign all element props to node
+
+// create nodes for children
+const text = document.createTextNode("");
+text["nodeValue"] = element.props.children;
